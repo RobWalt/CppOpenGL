f2571d0d04a4f8ca8eb648fe5474c2da56d04c58
RobWalt <robwalter96@gmail.com>
2021-01-30 16:40:32 +0100

    Adapted different Repository Structure

---
+++ EquationSolver/app/main.cpp
@@ -0,0 +1,31 @@
+// ----------- Template Stuff
+#include <iostream>
+
+#include "my_lib.hpp"
+#include "loguru.hpp"
+
+#include "ProjectConfig.h"
+// ------------
+
+// OpenGL Boilerplate Code Stuff
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
+#include "my_glm.hpp"
+#include "vertexshader.hpp"
+#include "physics.hpp"
+
+using namespace glm;
+ 
+int main( void )
+{
+    //DifferentialEquation<10> DEQ;
+
+    std::cout << SHADER_PATH << std::endl;
+
+    return 0;
+}
