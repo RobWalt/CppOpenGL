656a92272be8dd20994839cb4c933303af78a922
Solomon Jacobs <solomon.jacobs@tu-dresden.de>
2021-01-30 17:14:34 +0100

    Ignore build dir in Heatmap third attempt

--- openglHeatmap/build/src/CMakeFiles/utility.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/utility.dir/utilities.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/utility.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
