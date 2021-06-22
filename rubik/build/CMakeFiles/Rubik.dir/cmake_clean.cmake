c3ccc9a5a0cbc4a0380a065e943379307da02684
RobWalt <robwalter96@gmail.com>
2021-06-22 15:02:44 +0200

    Rubiks Cube Project Start

---
+++ rubik/build/CMakeFiles/Rubik.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rubik.dir/src/main.cpp.o"
+  "CMakeFiles/Rubik.dir/src/main.cpp.o.d"
+  "Rubik"
+  "Rubik.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rubik.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
